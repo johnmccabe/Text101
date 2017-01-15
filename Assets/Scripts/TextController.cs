f3c1d6e59ba25656a2c7343d4e25b776258a431f
John McCabe <john@johnmccabe.net>
2017-01-15 21:48:37 +0000

    word wrapping and joining strings

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -9,13 +9,20 @@ public class TextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		text.text = "Hello World";
+		text.text = "Press Space to begin your adventure...";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			text.text = "Space key pressed";
+			text.text = "You awaken groggily in the corner of a dark, dank room. " +
+						"As your eyes grow accustomed to the gloom you realise " +
+						"that you're in a dungeon cell.\n\n" +
+						"Throwing off the filthy sheets you climb out of bed, bolt " +
+						"to the cell door finding it unsurprisingly locked, turning " +
+						"back you startle at the sight of your blood stained face in " +
+						"the odd mirror hanging on the wall.\n\n" +
+						"Press S to view Sheets, M to view Mirror, or L for Lock.";
 		}
 	}
 }
